60513ced55d9f06906259393ea18daf26b838f36
jonathanbrierre <jonathanbrierre@yahoo.com>
2020-06-14 20:51:43 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const objA = {
+    1: 'hello',
+    2: 'bonjour',
+    3: 'hola',
+    4: 'aloha'
+}
+
+const objB = {
+    1: 'hello',
+    2: 'bonjour',
+    3: 'hola',
+    4: 'aloha'
+}
+
+let hellos = objA
+
+console.log(hellos)
+console.log(objA)
+console.log(objA === objB)
+
+
+
+
+
+
+
+
